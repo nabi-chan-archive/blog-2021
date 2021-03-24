91c9cb17715963439edaa9c58feccc62ffd44cce
PINOT <pinot.kim@kakao.com>
2021-03-25 04:31:58 +0900

    ✨ [목록] DB에서 Post를 불러오는 기능 추가
    - Post 타입 추가
    - Post model 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,38 @@
 import React from "react";
-import { NextPage } from "next";
+import { GetServerSideProps, NextPage } from "next";
+import prisma from "../lib/prisma";
+import { Post } from "../constants/type";
 
-const Home: NextPage = () => {
+interface Props {
+  posts: Post[];
+}
+
+export const getServerSideProps: GetServerSideProps<Props> = async (ctx) => {
+  const posts = await prisma.post.findMany({
+    orderBy: {
+      id: "desc",
+    },
+  });
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
+
+const Home: NextPage<Props> = ({ posts }) => {
   return (
     <div>
-      <h1>Hello, NextJS!</h1>
+      <h1>Feed</h1>
+
+      {posts.map((post) => (
+        <div key={post.id}>
+          <h2>{post.title}</h2>
+          <p>{post.body}</p>
+          <b>{post.createdAt.toString()}</b>
+        </div>
+      ))}
     </div>
   );
 };
