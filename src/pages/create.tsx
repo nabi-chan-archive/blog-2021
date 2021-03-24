33fd93f16e114e45920ed475db9e3688322b7c54
PINOT <pinot.kim@kakao.com>
2021-03-25 08:12:26 +0900

    💄 [작성] Form 디자인 추가
    - Label 컴포넌트 추가
    - Form 컴포넌트 추가
    - Container 추가

--- src/pages/create.tsx
+++ src/pages/create.tsx
@@ -1,5 +1,37 @@
 import React, { useState } from "react";
 import { NextPage } from "next";
+import styled from "styled-components";
+import Container from "../layouts/Container";
+
+const Form = styled.form`
+  display: grid;
+  grid-row-gap: 20px;
+`;
+
+const Label = styled.label`
+  display: block;
+
+  span {
+    color: #000;
+  }
+
+  input,
+  textarea {
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+    box-sizing: content-box;
+    border: solid 1px #a0a0a0;
+    width: calc(100% - 6ex);
+    padding: 3ex;
+    font-size: 12px;
+  }
+
+  textarea {
+    min-height: 30ex;
+    resize: vertical;
+  }
+`;
 
 const Create: NextPage = () => {
   const [title, setTitle] = useState<string>("");
@@ -30,28 +62,35 @@ const Create: NextPage = () => {
   };
 
   return (
-    <div>
+    <Container>
       <h1>Post 만들기</h1>
 
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          name="title"
-          placeholder={"제목을 입력하세요."}
-        />
-        <textarea
-          value={body}
-          onChange={(e) => setBody(e.target.value)}
-          name="body"
-          placeholder={"내용을 입력하세요."}
-        />
+      <Form onSubmit={handleSubmit}>
+        <Label>
+          <span>제목</span>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            name="title"
+            placeholder={"제목을 입력하세요."}
+          />
+        </Label>
+
+        <Label>
+          <span>본문</span>
+          <textarea
+            value={body}
+            onChange={(e) => setBody(e.target.value)}
+            name="body"
+            placeholder={"내용을 입력하세요."}
+          />
+        </Label>
 
         <button type={"submit"}>저장하기</button>
         <button type={"reset"}>리셋</button>
-      </form>
-    </div>
+      </Form>
+    </Container>
   );
 };
 
