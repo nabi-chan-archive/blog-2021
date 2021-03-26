dd894a7cf95f20ae95007ac557e3aa2ec269bf04
PINOT <pinot.kim@kakao.com>
2021-03-26 13:42:25 +0900

    💄 [상세] 페이지의 스타일 변경

--- src/pages/detail/[postId].tsx
+++ src/pages/detail/[postId].tsx
@@ -2,10 +2,12 @@ import React from "react";
 import { GetServerSideProps, NextPage } from "next";
 import prisma from "../../lib/prisma";
 import { Post } from "../../constants/type";
-import Container from "../../layouts/Container";
-import PageLink from "../../components/PageLink";
+import Header from "../../components/Header";
+import { Container, Nav, Row } from "react-bootstrap";
+import Link from "next/link";
 
 interface Props {
+  postId: number;
   post: Post;
 }
 
@@ -20,6 +22,7 @@ export const getServerSideProps: GetServerSideProps<Props> = async (ctx) => {
 
     return {
       props: {
+        postId: parseInt(postId as string),
         post,
       },
     };
@@ -33,17 +36,43 @@ export const getServerSideProps: GetServerSideProps<Props> = async (ctx) => {
   }
 };
 
-const Detail: NextPage<Props> = ({ post }) => {
+const Detail: NextPage<Props> = ({ postId, post }) => {
   return (
-    <Container>
-      <PageLink href="/">메인으로</PageLink>
+    <>
+      <Header />
+      <Container>
+        <header>
+          <h2>{post.title}</h2>
+          <h6>{post.createdAt.toString()}</h6>
+          <Row>
+            <Nav className="mr-auto" activeKey={"/"}>
+              <Nav.Item>
+                <Link href={"/"}>
+                  <Nav.Link href={"/"}>뒤로가기</Nav.Link>
+                </Link>
+              </Nav.Item>
+            </Nav>
 
-      <h1>
-        {post.title} ({post.id})
-      </h1>
+            <Nav>
+              <Nav.Item>
+                <Link href={`/modify/${postId}`}>
+                  <Nav.Link href={`/modify/${postId}`}>수정하기</Nav.Link>
+                </Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Link href={"#"}>
+                  <Nav.Link href={"#"}>삭제하기</Nav.Link>
+                </Link>
+              </Nav.Item>
+            </Nav>
+          </Row>
+        </header>
 
-      <p>{post.body}</p>
-    </Container>
+        <article>
+          <p>{post.body}</p>
+        </article>
+      </Container>
+    </>
   );
 };
 
