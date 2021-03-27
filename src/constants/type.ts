ef80a81d8f5afbe4f85bf335196f4d36ddf7edb2
PINOT <pinot.kim@kakao.com>
2021-03-27 23:20:01 +0900

    🏷️ User에 대한 타입 추가

--- src/constants/type.ts
+++ src/constants/type.ts
@@ -3,4 +3,17 @@ export interface Post {
   title: string;
   body: string;
   createdAt: string;
+  author: User;
+  authorId: number;
+}
+
+export interface User {
+  id: number;
+  name: string | null;
+  email: string | null;
+  emailVerified: Date | null;
+  image: string | null;
+  createdAt: Date;
+  updatedAt: Date;
+  posts: Post[];
 }
