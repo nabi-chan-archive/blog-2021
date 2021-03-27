2baa124735a516afc242453bf9f734c073b64904
PINOT <pinot.kim@kakao.com>
2021-03-27 22:00:07 +0900

    ⚡️ Session Provier 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,16 +3,17 @@ import { NextComponentType } from "next";
 import { AppContext, AppInitialProps, AppProps } from "next/app";
 import GlobalStyle from "../styles/GlobalStyle";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Provider } from "next-auth/client";
 
 const MyApp: NextComponentType<AppContext, AppInitialProps, AppProps> = ({
   Component,
   pageProps,
 }) => {
   return (
-    <>
+    <Provider session={pageProps.session}>
       <GlobalStyle />
       <Component {...pageProps} />
-    </>
+    </Provider>
   );
 };
 
