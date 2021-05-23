bcec3f4a227968860e31402ab27d6131821aa338
PINOT <pinot.kim@kakao.com>
2021-05-24 06:59:42 +0900

    📈 NODE_ENV와 관계 없이 항상 사용자를 추적하도록 변경

--- src/lib/mixpanel.ts
+++ src/lib/mixpanel.ts
@@ -1,11 +1,9 @@
 import mixpanel from "mixpanel-browser";
 mixpanel.init(process.env.MIXPANEL_KEY || "b0dca1d782df743cbd77f4c87ce8e107");
 
-const production = process.env.NODE_ENV === "production";
-
 const actions = {
   track: (name: MIX_TRACK, props: Record<string, unknown>) => {
-    if (production) mixpanel.track(name, props);
+    mixpanel.track(name, props);
   },
 };
 
