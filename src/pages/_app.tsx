572d5076b8f9a56b8372f289773a3492b9910339
PINOT <pinot.kim@kakao.com>
2021-06-01 20:31:19 +0900

    ✨ 페이지를 이동할 때마다 progressBar를 작동시키는 기능을 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,25 @@ import { AppContext, AppInitialProps, AppProps } from "next/app";
 import GlobalStyle from "../styles/GlobalStyle";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Provider } from "next-auth/client";
+import { Router } from "next/router";
+import _nProgress from "nprogress";
+
+const nProgress = _nProgress.configure({
+  showSpinner: false,
+});
+
+Router.events.on("routeChangeStart", (url) => {
+  console.debug("change route", url);
+  nProgress.start();
+});
+Router.events.on("routeChangeComplete", () => {
+  console.debug("change route complete");
+  nProgress.done();
+});
+Router.events.on("routeChangeError", (error) => {
+  console.debug("change route error", error);
+  nProgress.done();
+});
 
 const MyApp: NextComponentType<AppContext, AppInitialProps, AppProps> = ({
   Component,
