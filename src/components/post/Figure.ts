cc5c9d272e75075597716e81fbda5503f8032fff
PINOT <pinot.kim@kakao.com>
2021-06-01 23:53:29 +0900

    💄 figure에 style을 추가

---
+++ src/components/post/Figure.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Figure = styled.figure`
+  display: grid;
+  justify-items: center;
+  text-align: center;
+`;
+
+export const FigCaption = styled.figcaption`
+  display: inline-block;
+  margin-top: 0.5em;
+  font-size: 0.8em;
+  color: #6c757d;
+`;
