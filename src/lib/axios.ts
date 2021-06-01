63d99fb02635ae779021346e2a645c9b2b324f24
PINOT <pinot.kim@kakao.com>
2021-06-01 20:43:29 +0900

    ✨ axios에서 api 호출 시 progressBar를 작동하는 기능을 추가

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,8 +1,31 @@
 import _axios from "axios";
+import nProgress from "nprogress";
 
 const axios = _axios.create({
   baseURL: "/api",
   timeout: 10000, // timeout: 10sec
 });
+
+axios.interceptors.request.use(
+  (config) => {
+    nProgress.start();
+    return config;
+  },
+  (error) => {
+    nProgress.done();
+    return Promise.reject(error);
+  },
+);
+
+axios.interceptors.response.use(
+  (response) => {
+    nProgress.done();
+    return response;
+  },
+  (error) => {
+    nProgress.done();
+    return Promise.reject(error);
+  },
+);
 
 export default axios;
